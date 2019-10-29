5fd895e8f0ec934d806e6b7033cb81bcb2000a2e
yanchanglu <yanchanglu@bonc.com.cn>
2019-10-29 18:28:51 +0800

    feat(首页): 完成首页开发

---
+++ src/pages/GoodsList/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavigationBar from '@/components/NavigationBar';
+import styles from './index.scss';
+
+class GoodsList extends React.PureComponent {
+  render() {
+    return (
+      <div className={styles['goods-list-page']}>
+        
+      </div>
+    );
+  }
+}
+
+export default GoodsList;
